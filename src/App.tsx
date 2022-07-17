079ab71c655a34823c06d8f434e6720b52fb30f6
Vilija Mažeikė <vilija.setkute@gmail.com>
2022-07-17 14:20:05 +0300

    comments from files deleted

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,6 @@ import { StyledContainer } from "./App.styled";
 import useData from "./utils/hooks/useData";
 import dataOptions from './utils/dropdownData.json'
 
-/* dataOptions:
-    error:
-        primitiveString
-        primitiveNumber
-        primitiveBoolean
-        primitiveNull
-    render:
-        groupedArray
-        ungroupedArray
-        mixedArray
-        stringArray
-        standardObject
-*/
-
 const App = () => {
 
   const data = useData(dataOptions.mixedArray)
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -4,8 +4,6 @@ import DropdownItem from "./DropdownItem"
 import {
     StyledDropdownHeader, StyledDropdownList, StyledError, StyledChipWrapper, StyledChipItem, StyledChipText, FlexStyle, MarginStyle
 } from './dropdown.styled'
-// import useData from "../../utils/hooks/useData"
-// import dataOptions from '../../utils/dropdownData.json'
 import { useState } from "react"
 
 
@@ -18,7 +16,6 @@ export const Dropdown = ({data}: any) => {
 
     const [selectedItems, setSelectedItems] = useState([])
     const [showDropdownList, setDropdownList] = useState(false)
-    // const data = useData(dataOptions.ungroupedArray)
 
     const manageChip = (chipName: string) => {
         const filtered = selectedItems.filter((el: string) => el !== chipName)
